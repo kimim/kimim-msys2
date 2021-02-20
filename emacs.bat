37623bbf451cbdea64b8554812d7c11f36cd3f48
kimim <kimi.im@outlook.com>
2021-02-20 15:41:35 +0800

    finally we can set the environment for emacs to run

--- emacs.bat
+++ emacs.bat
@@ -2,4 +2,4 @@
 rem set HOME=%~dp0home
 set MSYS2_PATH_TYPE=inherit
 rem %~dp0\mingw64.exe runemacs
-%~dp0\\usr\\bin\\env MSYSTEM=MINGW64 /usr/bin/bash -lc emacs
+%~dp0\\usr\\bin\\env MSYSTEM=MINGW64 /usr/bin/bash -lc "source /etc/bash.bashrc && emacs"
