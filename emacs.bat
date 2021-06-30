a8c5d2b2b92191bd680831c39bd08d059b1c2434
kimim <kimi.im@outlook.com>
2021-07-01 07:56:12 +0800

    change emacs.bat and rime color theme

--- emacs.bat
+++ emacs.bat
@@ -1,5 +1,4 @@
 @echo off
-rem set HOME=%~dp0home
+set HOME=%~dp0home\\kimim
 set MSYS2_PATH_TYPE=strict
-%~dp0\mingw64.exe runemacs
-rem %~dp0\\usr\\bin\\env MSYSTEM=MINGW64 /usr/bin/bash -lc "source /etc/bash.bashrc && runemacs"
+%~dp0\\usr\\bin\\env MSYSTEM=MINGW64 /usr/bin/bash -lc "source /etc/bash.bashrc && runemacs"
