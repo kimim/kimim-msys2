9d0c5010514671986a4670e1bade9296fad8e20e
kimim <kimi.im@outlook.com>
2021-10-23 21:01:38 +0800

    use default mirrors

--- emacs.bat
+++ emacs.bat
@@ -3,4 +3,4 @@ rem change home path by adding below line
 rem set HOME=%USERPROFILE%\home
 set MSYS_ROOT=%~dp0
 set MSYS2_PATH_TYPE=strict
-%~dp0\\usr\\bin\\env MSYSTEM=MINGW64 /usr/bin/bash -lc "source /etc/bash.bashrc && /kimikit/emacs/bin/emacs"
+%~dp0\\usr\\bin\\env MSYSTEM=MINGW64 /usr/bin/bash -lc "source /etc/bash.bashrc && emacs"
--- emacsclient.bat
+++ emacsclient.bat
@@ -4,5 +4,5 @@ rem change home path by adding below line
 rem set HOME=%USERPROFILE%\home
 set MSYS_ROOT=%~dp0
 set MSYS2_PATH_TYPE=strict
-%~dp0\\usr\\bin\\env MSYSTEM=MINGW64 /usr/bin/bash -lc "source /etc/bash.bashrc && /kimikit/emacs/bin/emacsclientw -c"
-rem %~dp0\\usr\\bin\\env MSYSTEM=MINGW64 /usr/bin/bash -lc "source /etc/bash.bashrc && /kimikit/emacs/bin/emacsclientw $(cygpath %1)"
+%~dp0\\usr\\bin\\env MSYSTEM=MINGW64 /usr/bin/bash -lc "source /etc/bash.bashrc && emacsclientw -c"
+rem %~dp0\\usr\\bin\\env MSYSTEM=MINGW64 /usr/bin/bash -lc "source /etc/bash.bashrc && emacsclientw $(cygpath %1)"
