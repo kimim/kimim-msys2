001924594bc83f4821ae480f56174d035c8d475d
kimim <kimi.im@outlook.com>
2021-10-23 20:58:32 +0800

    update org-protocol

--- org-protocol.bat
+++ org-protocol.bat
@@ -1,6 +1,9 @@
 @echo off
+rem change home path by adding below line
+rem set HOME=%USERPROFILE%\home
+set MSYS2_PATH_TYPE=strict
 set URL=%1
 set URL=%URL:&=^&%
 set URL=%URL:/?=?%
 set URL=%URL:://=:///%
-start "" "D:\msys64\mingw64\bin\emacsclientw.exe" -na "D:\msys64\mingw64\bin\runemacs.exe" "%URL%"
+start "" "emacsclientw.exe" -na "runemacs.exe" "%URL%"
