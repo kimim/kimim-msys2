20c1fcc3194651e33f2838ee05d12ce72f1fea16
kimim <kimi.im@outlook.com>
2024-02-27 21:57:48 +0800

    remove scripts from msys2

--- kimikit/scripts/proxy-off.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Proxy Disable
-reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v "ProxyEnable" /t REG_DWORD /d 00000000 /f
-reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v AutoConfigURL /f
-echo Done
-pause
--- kimikit/scripts/proxy-on.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Proxy Enable
-reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v "ProxyEnable" /t REG_DWORD /d 00000001 /f
-reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v "ProxyServer" /t REG_SZ /d "localhost:1080" /f
-reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v "AutoConfigURL" /t REG_SZ /d "http://127.0.0.1:1080/pac?hash=xEfoJhjqxIj9o7meNJR60g2&secret=JQmY7qRMP8xxjG7pcIeXmHRFLv-yV2vuJs6MytAn57I1"/f
-echo Done
-pause
